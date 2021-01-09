567ed9419aa9e21d300ad5f0957176aecd72e17b
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-09 13:21:47 -0800

    even-fib-numbers: Start.

--- src/first5.rs
+++ src/first5.rs
@@ -10,3 +10,7 @@ pub fn multiples_of_3_and_5(bound: u32) -> u32 {
         .filter(|n| n.rem_euclid(3) == 0 || n.rem_euclid(5) == 0)
         .sum()
 }
+
+pub fn even_fib_numbers(bound: u64) -> u64 {
+    0
+}
