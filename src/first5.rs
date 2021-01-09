38f522466d35a3609944cd124f84cc6239ec24cc
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-09 13:34:18 -0800

    even-fib-numbers: Finish.

--- src/first5.rs
+++ src/first5.rs
@@ -7,10 +7,20 @@ pub fn multiples_of_3_and_5(bound: u32) -> u32 {
     // v.iter().sum::<u32>()
     (3..bound)
         .into_iter()
-        .filter(|n| n.rem_euclid(3) == 0 || n.rem_euclid(5) == 0)
+        .filter(|n| n % 3 == 0 || n % 5 == 0)
         .sum()
 }
 
 pub fn even_fib_numbers(bound: u64) -> u64 {
-    0
+    (0..)
+        .scan((0, 1), |s, _| {
+            *s = (s.1, s.0 + s.1);
+            if s.1 < bound {
+                Some(s.1)
+            } else {
+                None
+            }
+        })
+        .filter(|n| n % 2 == 0)
+        .sum::<u64>()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod first5;
 
 fn main() {
     println!("{}", first5::multiples_of_3_and_5(1000));
-    println!("{}", first5::even_fib_numbers(10));
+    println!("{}", first5::even_fib_numbers(4_000_000));
 }
