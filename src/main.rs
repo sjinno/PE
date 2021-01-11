757180ac4c887c0f0bcedc2626a4f82a4d7f378d
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-11 00:08:10 -0800

    Rename files.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,28 @@
 #[macro_use]
 mod macros;
 
-mod first_5;
-mod six_10;
-mod twentyone_25;
+mod pe_001_005;
+mod pe_006_010;
+mod pe_021_025;
 
 fn main() {
     // 1 - 5
-    // println!("{}", first_5::multiples_of_3_and_5(1000));
-    // println!("{}", first_5::even_fib_numbers(4_000_000));
-    // println!("{}", first_5::largest_prime_factor(600_851_475_143));
-    // println!("{}", first_5::largest_palindrome_product(3, 4));
+    // println!("{}", pe_001_005::multiples_of_3_and_5(1000));
+    // println!("{}", pe_001_005::even_fib_numbers(4_000_000));
+    // println!("{}", pe_001_005::largest_prime_factor(600_851_475_143));
+    // println!("{}", pe_001_005::largest_palindrome_product(3, 4));
 
     // // 6 - 10
-    // println!("{}", six_10::largest_product_in_a_series(13));
-    // println!("{}", six_10::special_pythagorean_triplet(1000));
+    // println!("{}", pe_006_010::largest_product_in_a_series(13));
+    // println!("{}", pe_006_010::special_pythagorean_triplet(1000));
 
     // 21 - 25
-    println!(
-        "{}",
-        twentyone_25::lexicographic_permutations(10, 1_000_000)
-    );
+    println!("{}", pe_021_025::lexicographic_permutations(10, 1_000_000));
     // for i in 1..7 {
     //     println!(
     //         "Count {}: {}.",
     //         i,
-    //         twentyone_25::lexicographic_permutations(3, i)
+    //         pe_021_025::lexicographic_permutations(3, i)
     //     );
     // }
 }
